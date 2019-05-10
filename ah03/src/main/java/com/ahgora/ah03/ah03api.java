9dcc66184dd650ede95b821f80e9296dc6896c02
caio.silva <caio.silva@ahgora.com.br>
2019-05-09 21:41:44 -0300

    fix java code

--- ah03/src/main/java/com/ahgora/ah03/ah03api.java
+++ ah03/src/main/java/com/ahgora/ah03/ah03api.java
@@ -4,16 +4,25 @@ import java.util.HashMap;
 
 public class ah03api {
 
-    HashMap<String, String> cadastro_funcionario(String matricula, String codEmpresa) {
-        HashMap<String, String> responseError = new HashMap<String, String>();
-        responseError.put("error", true);
-        responseError.put("matricula", matricula);
-        responseError.put("res", "Ops! Sua digital não ficou muito boa.");
+    public static HashMap<String, String> cadastro_funcionario(String matricula, String codEmpresa) {
 
-        HashMap<String, String> responseSuccess = new HashMap<String, String>();
-        responseSuccess.put("error", false);
-        responseSuccess.put("matricula", matricula);
-        responseSuccess.put("res", "Cadastro de João feito com sucesso!");
+        final String mat = matricula;
+
+        HashMap<String, String> responseError = new HashMap<String, String>() {
+            {
+                put("error", "true");
+                put("matricula", mat);
+                put("res", "Ops! Sua digital não ficou muito boa.");
+            }
+        };
+
+        HashMap<String, String> responseSuccess = new HashMap<String, String>() {
+            {
+                put("error", "false");
+                put("matricula", mat);
+                put("res", "Cadastro realizado com sucesso!");
+            }
+        };
 
         if (matricula == null || codEmpresa == null) {
             return responseError;
